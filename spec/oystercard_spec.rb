101d164caa1240eb63bc94eed888effc091628df
Benjamin Kearns <rapiidz1@gmail.com>
2018-10-08 15:02:59 +0000

    #5 added top_up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,4 +3,12 @@ describe Oystercard do
 	it "checks the balance" do
 		expect(subject.balance).to eq 0
 	end
+	
+	describe '#top_up' do 
+		it "tops up balance" do 
+			card = Oystercard.new
+			card.top_up(10)
+			expect(card.balance).to eq 10
+		end
+	end
 end
